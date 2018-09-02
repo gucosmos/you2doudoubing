c9e80c26c8e927bd6e140e009b6e65c5f9cacc97
gucosmos <32949775+gucosmos@users.noreply.github.com>
2018-09-02 19:11:03 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
 define('APIKEY', 'AIzaSyC405WOR6-KpdrPAOfKPyCUdLY8sA5H9hE');
-define('GJ_CODE', 'US');
+define('GJ_CODE', 'JP');
 define('SITE_NAME', 'YouTube');
 define('TITLENAME', 'YouTube');
-define('EN2DEKEY', 'lujianbupingyishenghou');
+define('EN2DEKEY', 'qwertyuio');
 define('EMAIL', 'gucosmos@protonmail.com');
 ?>
